f27d3707663c3d6306e9541cc717d71a3374093a
ruby04 <ruby04@SHUREE-MacBook-Air.local>
2023-05-12 13:45:06 +0900

    IWTR-42 #comment feat : models 구현
    - Update posts/models.py : posts 앱 모델 작성
    - Update posts/admin.py : admin site에 모델 등록
    Close #IWTR-42

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,12 @@
+# posts/admin.py
+
 from django.contrib import admin
+from .models import Artist, Gallery, Exhibition, Review
 
 # Register your models here.
+admin.site.register(Artist)
+admin.site.register(Gallery)
+admin.site.register(Exhibition)
+admin.site.register(Review)
+
+
