9436560b3148e49f982ee262da35fcdf07f56c32
ruby04 <ruby04@SHUREE-MacBook-Air.local>
2023-05-14 01:21:26 +0900

    IWTR-71 #comment feat : 관리자 기능 테스트 url, template 생성
    - Add staff.html to posts/templates/posts/test/
    - Move test templates to test directory
    - Add staff url to posts/urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,11 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:theme_pk>/', views.theme, name='theme'),
-    path('create/', views.create_theme, name='create_theme'),
+
+    # 이하 관리자만 접근 가능한 url 목록
+    path('staff/', views.staff, name='staff'),
+    path('staff/update_exhibition_list/', views.update_exhibition_list, name='update_exhibition_list'),
+    path('staff/create_theme/', views.create_theme, name='create_theme'),
     path('<int:theme_pk>/update/', views.update_theme, name='update_theme'),
     path('<int:theme_pk>/delete/', views.delete_theme, name='delete_theme'),
 ]
