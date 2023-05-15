595bbb78fd46d2210b7084735e30465a0756cfab
jeonseongjae98 <jeonseongjae@naver.com>
2023-05-15 15:23:07 +0900

    IWRT-76 IWRT-53 #comment feat : pw 변경, 회원정보수정 기능추가
    - 패스워드 변경, 회원정보 업데이트, 프로필, 팔로우 기능 추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,28 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
+from django.forms.widgets import ClearableFileInput
+from imagekit.forms import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ('email', 'nickname') 
+        fields = ('email', 'nickname',) 
 
-        #수정
+
+class CustomUserChangeForm(UserChangeForm):
+    profile_image = ProcessedImageField(
+        spec_id='profile_image_thumbnail',
+        processors=[ResizeToFill(70,70)],
+        format='JPEG',
+        options={'quality' : 90},
+        required=False,
+        widget=ClearableFileInput(
+        attrs={
+        }
+        ),
+    )
+    class Meta(UserChangeForm.Meta):
+        model = get_user_model()
+        fields = ('email', 'nickname', 'profile_image',)
+        
