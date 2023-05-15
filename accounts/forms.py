7bd7ce24c497e1cd00760994e83b37932bd6f659
doll2gom <shkim_ok@naver.com>
2023-05-15 11:57:09 +0900

    IWTR-59 #comment feat : 회원가입 기능 추가
    - Update accounts/signup.html : 회원가입 화면 구현
    - Update accounts/forms.py
      : LogInForm 생성
        LogInForm, CustomUserCreationForm 위젯 설정

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,39 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
+from django import forms
 
+# 로그인폼 위젯 
+class LogInForm(AuthenticationForm):
+    email = forms.EmailField(
+        label='이메일',
+        widget=forms.TextInput(
+            attrs={'class': 'form-control','placeholder': '이메일을 입력하세요',}),)
+    password = forms.CharField(
+        label='비밀번호',
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control','placeholder': '비밀번호를 입력하세요',}),)
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password')
+        
+# 회원가입폼 위젯
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(
+        label='이메일',
+        widget=forms.TextInput(
+            attrs={'class': 'form-control','placeholder': '이메일을 입력하세요',}),)
+    nickname = forms.CharField(
+        label='닉네임',
+        widget= forms.TextInput(
+            attrs={'class': 'form-control','placeholder': '닉네임을 입력하세요',}))
+    password1 = forms.CharField(
+        label='비밀번호',
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control','placeholder': '비밀번호를 입력하세요',}),)
+    password2 = forms.CharField(
+        label='비밀번호 확인',
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control','placeholder': '비밀번호를 다시 입력하세요',}),)    
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('email', 'nickname') 
