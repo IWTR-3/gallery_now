f6e9f5f6e956ae6c5c1049e0c303267c90e8dbec
ruby04 <ruby04@SHUREE-MacBook-Air.local>
2023-05-16 18:36:13 +0900

    IWTR-85 #comment chore : posts 앱 url 수정
    - Update urls according to following scheme
    
    | feature | url | view | name |
    |---|---|---|---|
    | 모든theme 조회 | '' | index | name='index' |
    | theme상세 조회 | 'post-list/<int:theme_pk>' | post_list | name='post_list' |
    | post상세 조회  | 'post/<int:post_pk>' | detail | name='detail' |
    | post 좋아요  | 'post/<int:post_pk>/like' | like | name='like' |
    | post 다녀옴  | 'post/<int:post_pk>/visited' | visited | name='visited' |
    | review 작성  | 'post/<int:post_pk>/review' | review | name='review' |
    | review 수정  | 'post/<int:post_pk>/review/<int:review_pk>/update' | review_update | name='review_update' |
    | review 삭제  | 'post/<int:post_pk>/review/<int:review_pk>/delete' | review_delete | name='review_delete' |
    | review 좋아요  | 'post/<int:post_pk>/review/<int:review_pk>/like' | review_like | name='review_like' |

--- posts/urls.py
+++ posts/urls.py
@@ -3,23 +3,25 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    
-    path('', views.index, name='index'),  # theme_list 조회
 
-    ## posts_list에서 theme로 명칭 변겅 ##
-    path('<int:theme_pk>', views.theme, name='theme'),  # theme detail 조회
-    
-    path('exhibition/<int:exhibition_pk>/', views.detail,
-         name='detail'),  # exhibition detail 조회
-    path('exhibition/<int:exhibition_pk>/review',
-         views.review, name='review'),  # review 작성
-    path('exhibition/<int:exhibition_pk>/review/<int:review_pk>/update/',
+    ## main features - post & theme ##
+    path('', views.index, name='index'),  # 모든 theme 조회
+    path('post-list/<int:theme_pk>', views.post_list,
+         name='post_list'),  # theme 상세
+    path('post/<int:post_pk>', views.detail, name='detail'),  # post detail 조회
+
+    ## like, visited ##
+    path('post/<int:post_pk>/like', views.like, name='like'),
+    path('post/<int:post_pk>/visited', views.visited, name='visited'),
+
+    ## review CRUD, like ##
+    path('post/<int:post_pk>/review', views.review, name='review'),  # review 작성
+    path('post/<int:post_pk>/review/<int:review_pk>/update',
          views.review_update, name='review_update'),  # review 수정
-    path('exhibition/<int:exhibition_pk>/review/<int:review_pk>/delete/',
+    path('post/<int:post_pk>/review/<int:review_pk>/delete',
          views.review_delete, name='review_delete'),  # review 삭제
-
-    path('theme2', views.theme2 , name='theme2'),
-    path('theme2/detail', views.detail2, name='detail2'),
+    path('post/<int:post_pk>/review/<int:review_pk>/like',
+         views.review_like, name='review_like'),  # review 좋아요
 
     # 이하 관리자만 접근 가능한 url 목록
     path('staff/', views.staff, name='staff'),
